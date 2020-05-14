616b8df0e1f25b2c40a72921b6e2625416795f2e
R-Ranito <r.ranito@hotmail.com>
2020-05-14 14:31:42 +0100

    First commit

---
+++ Game/src/input/KeyBoard.java
@@ -0,0 +1,41 @@
+package input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyBoard implements KeyListener{
+	
+	private boolean[] keys = new boolean[256];
+	
+	public static boolean UP, LEFT, RIGHT, SHOOT;
+	
+	public KeyBoard()
+	{
+		UP = false;
+		LEFT = false;
+		RIGHT = false;
+		SHOOT = false;
+	}
+	
+	public void update()
+	{
+		UP = keys[KeyEvent.VK_W];
+		LEFT = keys[KeyEvent.VK_A];
+		RIGHT = keys[KeyEvent.VK_D];
+		SHOOT = keys[KeyEvent.VK_SPACE];
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		keys[e.getKeyCode()] = true;
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		keys[e.getKeyCode()] = false;
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {}
+	
+}
